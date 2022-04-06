a5be03b4677c35dc67573519ef4fea3036fb623b
David Wang <david2000wang@gmail.com>
2022-04-06 17:59:21 -0400

    done

--- programs/solRust/src/lib.rs
+++ programs/solRust/src/lib.rs
@@ -1,6 +1,6 @@
 use anchor_lang::prelude::*;
 
-declare_id!("Fg6PaFpoGXkYsidMpWTK6W2BeZ7FEfcYkg476zPFsLnS");
+declare_id!("98PAdXWvGxzND7uWW9QXGhGxtzjNh4H2pctY29h39zRu");
 
 #[program]
 pub mod solrust {
